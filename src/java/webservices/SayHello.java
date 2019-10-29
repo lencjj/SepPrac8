72579807c4f982a8f0ae34bff54a96b1d7703c24
Lenc <Lenc@172.22.66.76>
2019-10-29 14:05:02 +0800

    Practical8_branch-Ex1.1 - addedQueryParam

--- src/java/webservices/SayHello.java
+++ src/java/webservices/SayHello.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -33,13 +34,14 @@ public class SayHello {
 
     /**
      * Retrieves representation of an instance of webservices.SayHello
+     * @param username
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String SayHello() {
+    public String SayHello(@QueryParam("username")String username) {
         //TODO return proper representation object
-        return "Hello there!";
+        return "Hello there!..." + username;
     }
 
     /**
